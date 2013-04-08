5848876e1be90b75340065fb5078baa7323b83f6
Salil Kapur <salilkapur93@gmail.com>
2013-04-08 20:08:53 +0530

    Linescan operators/ TV Denoising

--- SimpleCV/LineScan.py
+++ SimpleCV/LineScan.py
@@ -87,6 +87,52 @@ class LineScan(list):
         """
         return self.__getitem__(slice(i,j))
 
+    def __sub__(self,other):
+        
+        if len(self) == len(other):
+            retVal = LineScan(map(operator.sub,self,other))
+        else:
+            print 'Size mismatch'
+            return None
+        retVal._update(self)
+        return retVal
+
+    def __add__(self,other):
+        
+        if len(self) == len(other):
+            retVal = LineScan(map(operator.add,self,other))
+        else:
+            print 'Size mismatch'
+            return None
+        retVal._update(self)
+        return retVal
+
+    def __mul__(self,other):
+
+        if len(self) == len(other):
+            retVal = LineScan(map(operator.mul,self,other))
+        else:
+            print 'Size mismatch'
+            return None
+
+        retVal._update(self)
+        return retVal
+
+    def __div__(self,other):
+
+        if len(self) == len(other):
+            try:
+                retVal = LineScan(map(operator.div,self,other))
+            except ZeroDivisionError:
+                print 'Second LineScan contains zeros'
+                return None
+        else:
+            print 'Size mismatch'
+            return None
+
+        retVal._update(self)
+        return retVal
+
     def _update(self, linescan):
         """
         ** SUMMARY**
